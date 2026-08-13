2b3089b02aed5cbe5ea32be292610c7187123d80
KUMARAN <kumaran@KUMARANs-MacBook-Air.local>
2026-08-14 03:11:46 +0530

    Removed some files

--- Rag.py
+++
@@ -1,5 +0,0 @@
-data=[]
-with open('cat-facts.txt', 'r') as file:
-    data=file.readlines()
-    print(f"loaded {len(data)} entities")
-    print(data,end="")
--- rmDuplicate.py
+++
@@ -1,9 +0,0 @@
-lst=[1,1,2,2,3,3,4,5]
-new_list=[]
-for el in lst:
-    if el in new_list:
-        continue
-    else:
-        new_list.append(el)
-print(new_list)
-
